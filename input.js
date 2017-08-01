2fc6d2d99aac01000ea504b96b2791f8f5f66485
fabyus <fabyusfs@gmail.com>
2017-08-01 18:58:45 +0200

    implemented highlightable classes

--- input.js
+++ input.js
@@ -124,8 +124,8 @@
                     target = newInputSlot(input.id, target, label, 'bool', input).rbox;
                 }
                 target.appendChild(new Range().createContextualFragment(''
-                    + '<div id="i' + input.id + '" class="input_bool_pill color_' + (value ? "secondary" : "disabled") + '">'
-                        + '<div class="input_bool_ball" style="left:' + (value ? '50%' : '0%') + '"></div>'
+                    + '<div id="i' + input.id + '" class="input_bool_pill color_' + (value ? "secondary" : "disabled") + ' highlightable">'
+                        + '<div class="input_bool_ball color_bg" style="left:' + (value ? '50%' : '0%') + '"></div>'
                     + '</div>'));
                 input.element = document.getElementById("i"+input.id);
                 input.element.addEventListener("click", function(e) {
@@ -359,7 +359,7 @@
                 }
                 target.appendChild(new Range().createContextualFragment(''
                     + '<div id="i' + input.id + '" class="input_range">'
-                        + '<div class="input_range_slider color_secondary" style="width:' + ((input.value - input.min) * 220 / (input.max - input.min)) + 'px">'
+                        + '<div class="input_range_slider color_secondary highlightable" style="width:' + ((input.value - input.min) * 220 / (input.max - input.min)) + 'px">'
                             + '<div class="input_range_text">' + input.prefix + input.value + input.suffix + '</div>'
                         + '</div>'
                     + '</div>'));
@@ -461,19 +461,23 @@
                     if (this.value == 0) {
                         if(!loop){ //disable left button
                             this.lbutton.classList.toggle("color_secondary", false);
+                            this.lbutton.classList.toggle("highlightable", false);
                             this.lbutton.classList.toggle("color_disabled", true);
                         }
                     }else{ //enable left button
                         this.lbutton.classList.toggle("color_secondary", true);
+                        this.lbutton.classList.toggle("highlightable", true);
                         this.lbutton.classList.toggle("color_disabled", false);
                     }
                     if (this.value == this.values.length-1) {
                         if(!loop){ //disable right button
                             this.rbutton.classList.toggle("color_secondary", false);
+                            this.rbutton.classList.toggle("highlightable", false);
                             this.rbutton.classList.toggle("color_disabled", true);
                         }
                     }else{ //enable right button
                         this.rbutton.classList.toggle("color_secondary", true);
+                        this.rbutton.classList.toggle("highlightable", true);
                         this.rbutton.classList.toggle("color_disabled", false);
                     }
                 };
@@ -489,7 +493,7 @@
                 }
                 var semiproduct = ''
                         + '<div id="i' + input.id + '" class="input_horzenum">'
-                            + '<div class="input_horzenum_button input_horzenum_left '+ (input.value <= 0 && !input.loop ? 'color_disabled' : 'color_secondary') +'">'
+                            + '<div class="input_horzenum_button input_horzenum_left '+ (input.value <= 0 && !input.loop ? 'color_disabled' : 'color_secondary highlightable') +'">'
                                 + '<svg>'
                                     + '<polygon points="10,17 20,10 20,24"/>'
                                 + '</svg>'
@@ -502,7 +506,7 @@
                 target.appendChild(new Range().createContextualFragment(semiproduct
                                 + '</div>'
                             + '</div>'
-                            + '<div class="input_horzenum_button input_horzenum_right '+ (input.value >= input.values.length - 1 && !input.loop ? 'color_disabled' : 'color_secondary') +'">'
+                            + '<div class="input_horzenum_button input_horzenum_right '+ (input.value >= input.values.length - 1 && !input.loop ? 'color_disabled' : 'color_secondary highlightable') +'">'
                                 + '<svg>'
                                     + '<polygon points="10,10 20,17 10,24"/>'
                                 + '</svg>'
@@ -616,7 +620,7 @@
                     currentZIndex++;
                     var semiproduct = '<div class="input_vertenum_overlayscroller" style="z-index:' + currentZIndex + ';">';
                     for (var i = 0; i < this.values.length; i++) {
-                        semiproduct += '<div class="input_vertenum_value color_secondary">' + this.values[i] + '</div>';
+                        semiproduct += '<div class="input_vertenum_value color_secondary highlightable">' + this.values[i] + '</div>';
                     }
                     document.body.appendChild(new Range().createContextualFragment(semiproduct + '</div>'));
                     this.overlayScroller = document.getElementsByClassName("input_vertenum_overlayscroller")[0];
@@ -664,7 +668,7 @@
                 target.appendChild(new Range().createContextualFragment(semiproduct
                             + '</div>'
                         + '</div>'
-                        + '<div class="input_vertenum_button color_secondary">'
+                        + '<div class="input_vertenum_button color_secondary highlightable">'
                             + '<svg>'
                                 + '<polygon points="10,13 21,13 16,6"/>'
                                 + '<polygon points="10,21 21,21 16,28"/>'
@@ -903,7 +907,7 @@
                         that.beginEditing(e);
                     };
                     document.body.appendChild(new Range().createContextualFragment(''
-                        + '<div id="input_color_panel" class="input_color_panel color_primary non_highlightable" style="z-index:' + currentZIndex + '; top:'+(rect.top+rect.height+window.scrollY)+ 'px; left:'+(rect.left+window.scrollX)+'px;">'
+                        + '<div id="input_color_panel" class="input_color_panel color_primary" style="z-index:' + currentZIndex + '; top:'+(rect.top+rect.height+window.scrollY)+ 'px; left:'+(rect.left+window.scrollX)+'px;">'
                             + '<div class="input_color_wheelpanel"></div>'
                             + '<div class="input_color_switchpanel">'
                                 + '<div>RGB</div>'
